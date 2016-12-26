738f5811af673a4ba1bae0ad74e4f25541973463
Nafseer Kadiyaravida <nafseerk@gmail.com>
2016-12-26 20:22:56 +0530

    minor formatting

--- builder/Computer.java
+++ builder/Computer.java
@@ -11,37 +11,37 @@ public class Computer {
   private boolean isBluetoothEnabled;
 
   public String getHDD() {
-		return HDD;
-	}
+    return HDD;
+  }
 
-	public String getRAM() {
-		return RAM;
-	}
+  public String getRAM() {
+    return RAM;
+  }
 
-	public boolean isGraphicsCardEnabled() {
-		return isGraphicsCardEnabled;
-	}
+  public boolean isGraphicsCardEnabled() {
+    return isGraphicsCardEnabled;
+  }
 
-	public boolean isBluetoothEnabled() {
-		return isBluetoothEnabled;
-	}
+  public boolean isBluetoothEnabled() {
+    return isBluetoothEnabled;
+  }
 
   private Computer(ComputerBuilder builder) {
     this.HDD = builder.HDD;
-		this.RAM = builder.RAM;
-		this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
-		this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    this.RAM = builder.RAM;
+    this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+    this.isBluetoothEnabled = builder.isBluetoothEnabled;
   }
 
   public static class ComputerBuilder {
 
     //required parameters
     private String HDD;
-		private String RAM;
+    private String RAM;
 
-		// optional parameters
-		private boolean isGraphicsCardEnabled;
-		private boolean isBluetoothEnabled;
+    // optional parameters
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
 
     public ComputerBuilder(String hdd, String ram) {
       this.HDD = hdd;
