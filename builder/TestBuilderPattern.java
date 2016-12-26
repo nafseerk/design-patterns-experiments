1be923d43dde061d2fe699e3c9f344519fcd44f0
Nafseer Kadiyaravida <nafseerk@gmail.com>
2016-12-26 20:16:07 +0530

    Builder pattern

---
+++ builder/TestBuilderPattern.java
@@ -0,0 +1,11 @@
+package builder;
+
+import builder.Computer;
+
+public class TestBuilderPattern {
+
+  public static void main(String[] args) {
+    Computer comp = new Computer.ComputerBuilder("500 GB", "2 GB").setGraphicsCardEnabled(true).setBluetoothEnabled(true).build();
+  }
+
+}
