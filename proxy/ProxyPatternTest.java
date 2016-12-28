94c3fad512beb40ca0a795826c46a30ceb2b571f
Nafseer Kadiyaravida <nafseerk@gmail.com>
2016-12-28 23:57:48 +0530

    Added Proxy Pattern

---
+++ proxy/ProxyPatternTest.java
@@ -0,0 +1,17 @@
+package proxy;
+
+import proxy.CommandExecutor;
+import proxy.CommandExecutorProxy;
+
+public class ProxyPatternTest {
+
+  public static void main(String[] args){
+    CommandExecutor executor = new CommandExecutorProxy("Nafseer", "wrong_pwd");
+    try {
+      executor.runCommand("ls -ltr");
+      executor.runCommand(" rm -rf abc.pdf");
+    } catch (Exception e) {
+      System.out.println("Exception Message::"+e.getMessage());
+    }
+  }
+}
